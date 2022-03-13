3f08431232bf129de2f5749e9c832786e3636dc2
Kamil Szewc <kamil.szewc@gmail.com>
2022-03-13 17:42:09 +0100

    Added multiple new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,33 @@ import { LoginComponent } from './login/login.component';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatButtonModule} from "@angular/material/button";
 import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
+import { TasksComponent } from './tasks/tasks.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { SchemasComponent } from './schemas/schemas.component';
+import { FilesComponent } from './files/files.component';
+import { GroupsComponent } from './groups/groups.component';
+
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'groups', component: GroupsComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'resources', component: ResourcesComponent },
+  { path: 'schemas', component: SchemasComponent },
+  { path: 'files', component: FilesComponent },
+  { path: 'login', component: LoginComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    LoginComponent
+    LoginComponent,
+    TasksComponent,
+    ResourcesComponent,
+    SchemasComponent,
+    FilesComponent,
+    GroupsComponent
   ],
   imports: [
     BrowserModule,
@@ -28,8 +49,10 @@ import {ReactiveFormsModule} from "@angular/forms";
     HttpClientModule,
     MatFormFieldModule,
     MatButtonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
