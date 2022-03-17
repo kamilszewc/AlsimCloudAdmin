445629732b6560ce4773c1cd8d885b71f4f2b603
Kamil Szewc <kamil.szewc@gmail.com>
2022-03-17 16:23:22 +0100

    Fixed some bugs

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Router} from "@angular/router";
+import {TasksService} from "./tasks.service";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-tasks',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('allTasksPaginator') allTasksPaginator!: MatPaginator;
+  @ViewChild('allTasksTable', {read: MatSort}) allTasksSort!: MatSort;
+  allTasks = new MatTableDataSource<Task>([]);
+
+  displayedColumns: string[] = ['id', 'name', 'details'];
+
+  constructor(private tasksService: TasksService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.getAllTasks()
+  }
+
+  getAllTasks() {
+    this.tasksService.getAllTasks()
+      .subscribe(allTasks => {
+        this.allTasks = new MatTableDataSource<Task>(allTasks)
+        this.allTasks.sort = this.allTasksSort;
+        this.allTasks.paginator = this.allTasksPaginator;
+      })
+  }
+
+  goTo(id: number) {
+    this.router.navigate(['task', id]);
   }
 
 }
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {GlobalConstants} from "../common/global-constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  constructor() { }
+  apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.apiUrl = GlobalConstants.apiUrl;
+  }
+
+  getAllTasks() {
+    return this.http.get<Task[]>(this.apiUrl + "/api/v1/task/findTasks?userId=5")
+  }
+
 }
