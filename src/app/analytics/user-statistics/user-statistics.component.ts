6350d9af3b87065b3410aaf6f31dc84e52d0ec80
Kamil Szewc <kamil.szewc@gmail.com>
2022-04-21 20:22:07 +0200

    Added plots in user-statistics

--- src/app/analytics/user-statistics/user-statistics.component.ts
+++ src/app/analytics/user-statistics/user-statistics.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {ChartDataset, ChartOptions, ChartType} from "chart.js";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {BubbleDataPoint, ChartDataset, ChartOptions, ChartType, ScatterDataPoint} from "chart.js";
+import {HeaderDescription, UserStatisticsService} from "./user-statistics.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-user-statistics',
@@ -8,28 +10,92 @@ import {ChartDataset, ChartOptions, ChartType} from "chart.js";
 })
 export class UserStatisticsComponent implements OnInit {
 
-  public chartData: ChartDataset[] = [
-    {
-      data: [
-        {x: 1, y: 3},
-        {x: 2, y: 4},
-        {x: 3, y: 3},
-        {x: 4, y: 6},
-        ],
-      label: 'Series A',
-      pointRadius: 5
-    }
-  ];
+  @ViewChild('timeRangePickerForm') timeRangePickerForm!: NgForm;
+  timeRangeOptions: Map<string, number | null>;
+  timeRangeNames!: string[];
 
-  public chartOptions: ChartOptions = {
-    responsive: true
+  chosenTimeRange = "since beginning";
+
+  public headers!: string[];
+  public chartDatasets!: Map<string, ChartDataset[]>;
+  public chartLabels!: Map<string, string[]>;
+  public chartOptions!: Map<string, ChartOptions>;
+  public chartType: ChartType = 'line';
+
+  constructor(private userStatisticsService: UserStatisticsService) {
+    this.timeRangeOptions = new Map<string, number | null>();
+    this.timeRangeOptions.set("since beginning", null)
+    this.timeRangeOptions.set("last week", 7)
+    this.timeRangeOptions.set("last month", 31)
+    this.timeRangeOptions.set("last year", 365)
+    this.timeRangeNames = Array.from(this.timeRangeOptions.keys());
   }
 
-  public chartType: ChartType = 'scatter';
-
-  constructor() { }
-
   ngOnInit(): void {
+    this.getPlotsData(null);
+  }
+
+  getPlotsData(timeRange: number | null): void {
+    this.chartDatasets = new Map<string, ChartDataset[]>()
+    this.chartLabels = new Map<string, string[]>()
+    this.chartOptions = new Map<string, ChartOptions>()
+
+    this.userStatisticsService.generateAnalyticsGrabberToken().subscribe(response => {
+      let token = response.message;
+      console.log(token)
+
+      this.userStatisticsService.getHeadersForPlotting(token).subscribe(headers => {
+
+        this.headers = [];
+
+        for (const [key, value] of Object.entries(headers)) {
+
+          this.headers.push(key);
+
+          this.userStatisticsService.getDataForPlotting(key, token, timeRange).subscribe(plot => {
+
+            let x = plot.x;
+            let y = plot.y;
+
+            const chartDataset : ChartDataset[] = [
+              {
+                data: y,
+                label: plot.title,
+                pointRadius: 5
+              }
+            ];
+
+            let chartOption: ChartOptions = {
+              responsive: true,
+              scales: {
+                x: {
+                  title: {
+                    display: true,
+                    text: plot.xlabel
+                  }
+                },
+                y: {
+                  title: {
+                    display: true,
+                    text: plot.ylabel
+                  }
+                }
+              }
+            }
+
+            this.chartDatasets.set(key, chartDataset);
+            this.chartLabels.set(key, x);
+            this.chartOptions.set(key, chartOption);
+          })
+        }
+
+      })
+    })
+  }
+
+  selectedTimeRange() {
+    const timeRange = this.timeRangeOptions.get(this.chosenTimeRange);
+    this.getPlotsData(timeRange!)
   }
 
 }
--- src/app/analytics/user-statistics/user-statistics.service.ts
+++ src/app/analytics/user-statistics/user-statistics.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {GlobalConstants} from "../../common/global-constants";
+import {Message} from "../../message";
+
+export interface HeaderDescription {
+  id: number;
+  name: string;
+  description: string;
+  button: string;
+}
+
+export interface TimeValueData {
+  x: string[];
+  y: number[];
+  title: string;
+  xlabel: string;
+  ylabel: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserStatisticsService {
 
-  constructor() { }
+  apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.apiUrl = GlobalConstants.apiUrl;
+  }
+
+  generateAnalyticsGrabberToken() {
+    return this.http.get<Message<string>>(this.apiUrl + "/api/v1/token/generateAnalyticsGrabberToken");
+  }
+
+  getHeadersForPlotting(token: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        Authorization: 'Bearer ' + token
+      })
+    };
+
+    return this.http.get<Map<string, HeaderDescription>>(this.apiUrl + "/api/v1/analytics/user/getHeadersForPlotting", httpOptions);
+  }
+
+  getDataForPlotting(header: string, token: string, timeRange: number | null) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        Authorization: 'Bearer ' + token
+      })
+    };
+
+    let parameter = ""
+    if (timeRange != null) {
+      parameter = "?timeRange" + timeRange;
+    }
+
+    return this.http.get<TimeValueData>(this.apiUrl + "/api/v1/analytics/user/getDataForPlotting/" + header + parameter, httpOptions);
+  }
 }
