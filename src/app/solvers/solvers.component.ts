7403e8ca27ede98c26a9b6306809c24d3b50da2b
Kamil Szewc <kamil.szewc@gmail.com>
2022-05-24 13:20:39 +0200

    Added solvers manipulation section

--- src/app/solvers/solvers.component.ts
+++ src/app/solvers/solvers.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {Solver, SolversService} from "./solvers.service";
+import {AlarmDialogMessage, AlarmDialogService} from "../alarm-dialog/alarm-dialog.service";
 
 @Component({
   selector: 'app-solvers',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolversComponent implements OnInit {
 
-  constructor() { }
+  solvers!: Solver[]
+  isLoading = true;
+
+  constructor(private router: Router,
+              private solversService: SolversService,
+              private route: ActivatedRoute,
+              private alarmDialogService: AlarmDialogService) { }
 
   ngOnInit(): void {
+    this.getSolvers();
+  }
+
+  getSolvers() {
+    this.solversService.generateSolversRepositoryToken().subscribe(message => {
+      // Get token
+      let token = message.message;
+
+      // Get input files
+      this.solversService.getSolvers(token).subscribe(solvers => {
+        this.solvers = solvers;
+        this.isLoading = false;
+      })
+    })
+  }
+
+  removeVersion(solver: string, version: string) {
+    this.solversService.generateSolversRepositoryToken().subscribe(message => {
+      // Get token
+      let token = message.message;
+
+      // Get input files
+      this.solversService.removeVersion(solver, version, token).subscribe(message => {
+        this.getSolvers()
+        const dialogMessage = new class implements AlarmDialogMessage {
+          title = "Information"
+          message = "Version " + version + " of solver " + solver + " removed"
+        };
+        this.alarmDialogService.open(dialogMessage);
+      })
+    })
   }
 
 }
--- src/app/solvers/solvers.service.ts
+++ src/app/solvers/solvers.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {GlobalConstants} from "../common/global-constants";
+import {Message} from "../message";
+
+export interface Solver {
+  name: string | null;
+  versions: Version[] | null;
+}
+
+export interface Version {
+  version: string | null;
+  solver: string | null;
+  size: number | null;
+  updateDateTime: string | null;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SolversService {
 
-  constructor() { }
+  apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.apiUrl = GlobalConstants.apiUrl;
+  }
+
+  generateSolversRepositoryToken() {
+    return this.http.get<Message<string>>(this.apiUrl + "/api/v1/token/generateSolversRepositoryToken");
+  }
+
+  getSolvers(token: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        Authorization: 'Bearer ' + token
+      })
+    };
+
+    return this.http.get<Solver[]>(this.apiUrl + "/api/v1/solvers", httpOptions);
+  }
+
+  removeVersion(solver: string, version: string, token: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        Authorization: 'Bearer ' + token
+      })
+    };
+
+    return this.http.delete<string>(this.apiUrl + "/api/v1/solvers/" + solver + "/" + version, httpOptions);
+  }
 }
