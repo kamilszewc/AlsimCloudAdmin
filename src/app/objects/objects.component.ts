c77558a32b185b490f83e54cd154901db2214632
Kamil Szewc <kamil.szewc@gmail.com>
2022-05-24 14:22:24 +0200

    Some preliminary implementations of objects repository

--- src/app/objects/objects.component.ts
+++ src/app/objects/objects.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {StorageService} from "../storage/storage.service";
+import {ObjectsService} from "./objects.service";
+import {MatTableDataSource} from "@angular/material/table";
+import {StorageFile} from "../storageFile";
 
 @Component({
   selector: 'app-objects',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+              private objectsService: ObjectsService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getGroups()
+  }
+
+  getGroups() {
+    this.objectsService.generateObjectsRepositoryToken().subscribe(message => {
+      // Get token
+      let token = message.message;
+
+      this.objectsService.getGroups(token).subscribe(groups => {
+        // this.inputFiles = new MatTableDataSource<StorageFile>(files);
+        // this.inputFiles.paginator = this.inputFilesPaginator;
+        // this.inputFiles.sort = this.inputFilesSort;
+        console.log(groups)
+      });
+    });
   }
 
 }
--- src/app/objects/objects.service.ts
+++ src/app/objects/objects.service.ts
@@ -1,9 +1,82 @@
 import { Injectable } from '@angular/core';
+import {GlobalConstants} from "../common/global-constants";
+import {Message} from "../message";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+export interface Simulation {
+  id: string;
+  time: number;
+  size: number;
+  checksum: string;
+  creationTime: string;
+  modificationTime: string;
+  url: string;
+}
+
+export interface ObjectUrl {
+  object: string;
+  thumbnail: string;
+  icon: string;
+  source: string;
+}
+
+export interface Object {
+  id: string;
+  name: string;
+  description: string;
+  extension: string;
+  extensionIcon: string;
+  extensionsThumbnail: string;
+  extensionSource: string;
+  hasSource: boolean;
+  owner: string;
+  permission: string;
+  isPublic: string;
+  size: number;
+  checksum: string;
+  creationTime: string;
+  modificationTime: string;
+  objectUrl: ObjectUrl;
+  simulations: Simulation[] | null;
+}
+
+export interface Group {
+  id: string;
+  description: string;
+  owner: string;
+  permission: string;
+  isPublic: boolean;
+  quota: number;
+  size: number;
+  creationTime: string;
+  modificationTime: string;
+  files: Object[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ObjectsService {
 
-  constructor() { }
+  apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.apiUrl = GlobalConstants.apiUrl;
+  }
+
+  generateObjectsRepositoryToken() {
+    return this.http.get<Message<string>>(this.apiUrl + "/api/v1/token/generateObjectsRepositoryToken");
+  }
+
+  getGroups(token: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        Authorization: 'Bearer ' + token
+      })
+    };
+
+    return this.http.get<Group[]>(this.apiUrl + "/api/v1/objectsrepository/groups")
+  }
+
 }
