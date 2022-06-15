2913f6bc60c09af33c28bff3e25f8ffa4d08bbc7
Kamil Szewc <kamil.szewc@gmail.com>
2022-06-15 13:39:15 +0200

    Added license-option manipulations

--- src/app/license-option/license-option.component.ts
+++ src/app/license-option/license-option.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {AlarmDialogService} from "../alarm-dialog/alarm-dialog.service";
+import {LicenseOptionService} from "./license-option.service";
+import {NgForm} from "@angular/forms";
+import {LicenseType} from "../license-types/license-types.service";
+import {LicenseOption} from "./license-option";
 
 @Component({
   selector: 'app-license-option',
@@ -7,9 +13,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LicenseOptionComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+
+  @ViewChild('licenseOptionForm') licenseOptionForm!: NgForm;
+  licenseOption!: LicenseOption;
+
+  isEditAllowed = false;
+  isRemovalAllowed = false;
+
+  clientTypes = ["ANY", "PRIVATE", "ENTERPRISE"]
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private licenseOptionService: LicenseOptionService,
+              private alarmDialogService: AlarmDialogService) { }
 
   ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.getLicenseOption()
   }
 
+  getLicenseOption() {
+    this.licenseOptionService.getLicenseOption(this.id!).subscribe(licenseOption => {
+      this.licenseOption = licenseOption;
+    })
+  }
+
+  allowEdit() {
+    this.isEditAllowed = true;
+  }
+
+  reload() {
+    window.location.reload();
+  }
+
+  editLicenseOption() {
+    this.licenseOptionService.editLicenseOption(this.id!, this.licenseOption!).subscribe(licenseOption => {
+        this.licenseOption = licenseOption;
+        window.location.reload()
+      }
+    )
+  }
+
+  allowRemoval() {
+    this.isRemovalAllowed = true;
+  }
+
+  @ViewChild('deleteButton') deleteButton!: HTMLButtonElement;
+  deleteLicenseType() {
+    this.deleteButton.disabled = true;
+    this.licenseOptionService.deleteLicenseOption(this.id!).subscribe(
+      message => {
+        this.router.navigate(['license-type', this.licenseOption.licenseTypeId]);
+      },
+      error => {
+        this.deleteButton.disabled = false;
+      }
+    )
+  }
 }
--- src/app/license-option/license-option.service.ts
+++ src/app/license-option/license-option.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {GlobalConstants} from "../common/global-constants";
+import {LicenseType} from "../license-types/license-types.service";
+import {LicenseOption} from "./license-option";
+import {Message} from "../message";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LicenseOptionService {
 
-  constructor() { }
+  apiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.apiUrl = GlobalConstants.apiUrl;
+  }
+
+  getLicenseOption(id: number) {
+    return this.http.get<LicenseOption>(this.apiUrl + "/api/v1/licenseOption/" + id);
+  }
+
+  editLicenseOption(id: number, licenseOption: LicenseOption) {
+    return this.http.put<LicenseOption>(this.apiUrl + "/api/v1/licenseOption/" + id, licenseOption);
+  }
+
+  deleteLicenseOption(id: number) {
+    return this.http.delete<Message<string>>(this.apiUrl + "/api/v1/licenseOption/" + id);
+  }
 }
